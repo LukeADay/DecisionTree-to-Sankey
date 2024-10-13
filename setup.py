7dfc61820b0a6501c2663e7d4beacb1100a275f2
lad1u13 <lukeday_10@hotmail.com>
2024-10-13 15:49:15 +0100

    Set up to convert into a library

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='decisiontree_to_sankey',
+    version='0.1',
+    description='A package to visualize decision trees as Sankey diagrams using Plotly.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Luke Day',
+    author_email='luke.alexander.day@gmail.com',
+    url='https://github.com/LukeADay/DecisionTree-to-Sankey',
+    packages=find_packages(where='src'),  # Look for packages in the 'src' directory
+    package_dir={'': 'src'},  # Tells setuptools where to find your package code
+    install_requires=[
+        'pandas',
+        'numpy',
+        'scikit-learn',
+        'plotly',
+        'seaborn',
+    ],
+    python_requires='>=3.6',
+)
