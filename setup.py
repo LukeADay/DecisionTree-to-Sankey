ba63854e28d862a6248548f796aac52d43c1dc3e
lad1u13 <lukeday_10@hotmail.com>
2024-10-13 19:46:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='DecisionTree_to_Sankey',
+    name='decisiontree-to-sankey',
     version='0.1',
     description='A package to visualize decision trees as Sankey diagrams using Plotly.',
     long_description=open('README.md').read(),
@@ -9,8 +9,8 @@ setup(
     author='Luke Day',
     author_email='luke.alexander.day@gmail.com',
     url='https://github.com/LukeADay/DecisionTree-to-Sankey',
-    packages=find_packages(where='src'),  # Look for packages in the 'src' directory
-    package_dir={'': 'src'},  # Tells setuptools where to find your package code
+    packages=find_packages(where='src'),  # Find packages in the 'src' folder
+    package_dir={'': 'src'},              # The root of the package is in the 'src' folder
     install_requires=[
         'pandas',
         'numpy',
